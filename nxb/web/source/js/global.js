75c75eb425d346feb12ccef66296026532a8def3
Dude <ddlbaf@gmail.com>
2016-08-04 22:30:14 +0800

    gzh
    更新  公众号绑页面独立

--- nxb/web/source/js/global.js
+++ nxb/web/source/js/global.js
@@ -11,6 +11,7 @@ define(function(require, exports, module) {
         AJAX: 'http://wx.gupiaoxianji.com/wxh5api/',                    //ajax请求
         CLUB: 'http://wx.gupiaoxianji.com/auth/indexcallbackclub/',  //会员
         INDEX: 'http://wx.gupiaoxianji.com/auth/indexcallbackfund/',  //默认跳转地址
+        BIND: 'http://wx.gupiaoxianji.com/auth/indexcallbackbind/'      //绑定帐号跳转地址
     };
 
     $.Func = {
@@ -193,6 +194,8 @@ define(function(require, exports, module) {
                     }
                 }
             });
+
+            $.Func.cookie.setCookie('gupiaoxianji_location', '');
         },
         //分享
         share: function () {
@@ -231,7 +234,6 @@ define(function(require, exports, module) {
             }
 
             wx.ready(function(){
-                console.log(1);
                 // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
 
                 //分享到朋友圈
